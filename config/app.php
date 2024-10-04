41cab7d99b20772de704b9f6f8de224b218a5d5e
Evan Ward <Evan29Ward@gmail.com>
2024-10-04 14:45:17 +0100

    Created alias for HTTP status codes

--- config/app.php
+++ config/app.php
@@ -123,4 +123,7 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'aliases' => \Illuminate\Support\Facades\Facade::defaultAliases()->merge([
+        'HttpStatus' => \Symfony\Component\HttpFoundation\Response::class,
+    ])->toArray(),
 ];
