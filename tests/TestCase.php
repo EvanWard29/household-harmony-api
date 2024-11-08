67f6ec21fd9222b9d47b6db52d0bac759ecbc799
Evan Ward <Evan29Ward@gmail.com>
2024-11-08 14:44:14 +0000

    Setup `TestCase::actingAs()` to use Sanctum

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    protected bool $seed = true;
+
+    public function actingAs(UserContract $user, $guard = null, array $abilities = ['*']): static
+    {
+        Sanctum::actingAs($user, $abilities, 'api');
+
+        return $this;
+    }
 }
