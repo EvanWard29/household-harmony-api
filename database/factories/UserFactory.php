2f4f83243ab0e14543f0369a177c9272ed038033
Evan Ward <Evan29Ward@gmail.com>
2024-11-08 14:43:48 +0000

    Modified the `UserFactory`

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,43 +2,54 @@
 
 namespace Database\Factories;
 
+use App\Models\Household;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => $firstName = fake()->firstName(),
+            'last_name' => $lastName = fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
+            'username' => $firstName.$lastName,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => \Hash::make('password123!!'),
+            'is_active' => true,
+
+            'household_id' => Household::factory(),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Mark the user's email as unverified
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+
+    /**
+     * Create a token for the user
+     */
+    public function token($token = 'token'): static
+    {
+        return $this->afterCreating(fn (User $user) => $user->createToken($token));
+    }
+
+    public function configure(): static
+    {
+        return $this->afterCreating(function (User $user) {
+            // Set the user as the owner of their household
+            $user->household->update(['owner_id' => $user->id]);
+        });
     }
 }
