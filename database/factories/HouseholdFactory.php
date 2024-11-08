15ae06e82f721f41b857ca94bc3c364410b3f1f9
Evan Ward <Evan29Ward@gmail.com>
2024-11-08 14:43:30 +0000

    Added `HouseholdFactory`

---
+++ database/factories/HouseholdFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Household;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class HouseholdFactory extends Factory
+{
+    protected $model = Household::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+
+            'owner_id' => null,
+        ];
+    }
+}
