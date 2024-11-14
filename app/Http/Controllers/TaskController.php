7626a1c525f84c8be455cc97fdb9cd2cb3a3e145
Evan Ward <Evan29Ward@gmail.com>
2024-11-14 13:51:42 +0000

    Tasks can now be filtered

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\TaskStatusEnum;
 use App\Http\Requests\TaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Household;
 use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class TaskController
 {
@@ -15,9 +20,61 @@ class TaskController
     /**
      * Retrieve all tasks of the given household
      */
-    public function index(Household $household)
+    public function index(Request $request, Household $household)
     {
-        return TaskResource::collection($household->tasks);
+        $request->validate([
+            'status' => [Rule::enum(TaskStatusEnum::class)],
+
+            'deadline.start.date' => ['date_format:Y-m-d', 'required_with:deadline'],
+            'deadline.start.time' => ['date_format:H:i'],
+
+            'deadline.end.date' => ['date_format:Y-m-d', 'required_with:deadline'],
+            'deadline.end.time' => ['date_format:H:i'],
+
+            'assigned' => ['array'],
+            'assigned.*' => [
+                'int',
+                Rule::exists(User::class, 'id')->where('household_id', $household->id),
+            ],
+        ]);
+
+        $tasks = $household->tasks();
+
+        // Filter tasks by status
+        if ($request->filled('status')) {
+            $tasks = $tasks->where('status', $request->enum('status', TaskStatusEnum::class));
+        }
+
+        // Filter tasks between the set date and time deadline
+        if ($request->filled('deadline')) {
+            $start = $request->date('deadline.start.date');
+            $end = $request->date('deadline.end.date');
+
+            if ($request->filled('deadline.start.time')) {
+                $start->setTimeFrom($request->date('deadline.start.time'));
+            } else {
+                $start->startOfDay();
+            }
+
+            if ($request->filled('deadline.end.time')) {
+                $end->setTimeFrom($request->date('deadline.end.time'));
+            } else {
+                $end->endOfDay();
+            }
+
+            $tasks = $tasks->whereBetween('deadline', [$start, $end]);
+        }
+
+        // Filter tasks by assignee
+        if ($request->filled('assigned')) {
+            $tasks = $tasks->whereHas('assigned', function (Builder $query) use ($request) {
+                $query->whereIn('id', $request->input('assigned'));
+            });
+        }
+
+        $tasks->with('assigned.roles');
+
+        return TaskResource::collection($tasks->get());
     }
 
     /**
