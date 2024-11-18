ba127241a32a440ddc947b2dd8bfa0686eaabf01
Evan Ward <Evan29Ward@gmail.com>
2024-11-18 17:33:12 +0000

    Added group to TaskFactory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Enums\TaskStatusEnum;
+use App\Models\Group;
 use App\Models\Household;
 use App\Models\Task;
 use App\Models\User;
@@ -18,9 +19,14 @@ class TaskFactory extends Factory
             'title' => fake()->words(asText: true),
             'description' => fake()->text(),
             'status' => TaskStatusEnum::TODO,
-            'deadline' => now()->addDays(3),
+            'deadline' => now()->addDays(3)->startOfMinute(),
+            'group_id' => function (array $attributes) {
+                return Group::factory()->for(Household::findOrFail($attributes['household_id']));
+            },
 
-            'owner_id' => User::factory(),
+            'owner_id' => function (array $attributes) {
+                return User::factory()->for(Household::findOrFail($attributes['household_id']));
+            },
             'household_id' => Household::factory(),
         ];
     }
