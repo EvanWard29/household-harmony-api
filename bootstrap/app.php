d106b22c9878fe2900b6bfcc54307ecc52459e3a
Evan Ward <Evan29Ward@gmail.com>
2024-12-04 15:37:48 +0000

    Created `subscription` middleware to restrict routes to subscribed users

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\PasswordConfirmationMiddleware;
+use App\Http\Middleware\SubscriptionMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(['password.confirm' => PasswordConfirmationMiddleware::class]);
+        $middleware->alias(['subscribed' => SubscriptionMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
